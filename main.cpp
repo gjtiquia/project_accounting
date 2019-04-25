1755bdc5b3dac41aac2830db7824c57b3867140e
LauKwaiLam <47241419+LauKwaiLam@users.noreply.github.com>
2019-04-25 12:16:18 +0800

    Update main.cpp
    create function 3 and 4

--- main.cpp
+++ main.cpp
@@ -245,7 +245,7 @@ void mode_delete(string username) {
   }
   fin.close();
   
-  cout << "How many record you want to delete? " << endl;
+  cout << "How many record you want to delete? " ;
   int num;
   cin >> num;
   for (int k=0; k<num ;k++){
@@ -254,10 +254,15 @@ void mode_delete(string username) {
     cout << "Find the record that you want to delete by"<<endl;
     cout << "1) amount"<<endl;
     cout << "2) date" <<endl;
+    cout << "0) stop delete" << endl;
     
-    int command;
-    cin >> command;
-    if (command==1){
+    string command;
+    enterData(command);
+    while (not(command =="1" or command=="2"  or command =="0")){
+      cout << "Invalid command, please input it again: ";
+      cin >> command;
+    }
+    if (command=="1"){
       
       //input range
       double max, min;
@@ -290,7 +295,7 @@ void mode_delete(string username) {
         cin >> recordtodelete;
         recordtodelete--;
       
-    }else if (command ==2){
+    }else if (command =="2"){
       
       //input target date
       int numberoutput = 0;
@@ -320,6 +325,8 @@ void mode_delete(string username) {
       cin >> recordtodelete;
       recordtodelete--;
       
+    }else if (command == "0"){
+      break;
     }
     //delete the record choosen
     deletefromdoublearray(amount, numberofdata, recordtodelete);
@@ -336,7 +343,8 @@ void mode_delete(string username) {
     fout << amount[i] << ' ' << date[i] <<' '<< time[i] <<' '<< type[i] <<' '<< account[i] <<endl;
   }
   fout.close();
-    
+  
+  cout << "Record deleted successfully" << endl;
     
     
   }
@@ -354,7 +362,227 @@ void mode_delete(string username) {
 // Function: (3)
 // Input: string username: user's username
 void mode_edit(string username) {
-  cout << "EDIT mode selected" << endl;  
+  cout << "EDIT mode selected" << endl;
+  
+  ifstream fin(username + ".txt");
+  int numberofdata;
+  fin >> numberofdata;
+  
+  //create 4 dynamic arrays
+  double * amount = new double [numberofdata];
+  string * date = new string [numberofdata];
+  string * time = new string [numberofdata];
+  string * type = new string [numberofdata];
+  string * account = new string [numberofdata];
+  
+  //input all data into array
+  for (int i=0; i<numberofdata; i++){
+    fin >> amount[i];
+    fin >> date[i];
+    fin >> time[i];
+    fin >> type[i];
+    fin >> account[i];  
+  }
+  fin.close();
+  
+  cout << "How many record you want to edit? " << endl;
+  int num;
+  cin >> num;
+  for (int k=0; k<num ;k++){
+    int recordtoedit;
+    cout << "Number of record to edit: " << num-k << endl;
+    cout << "Find the record that you want to edit by"<<endl;
+    cout << "1) amount"<< endl;
+    cout << "2) date" << endl;
+    cout << "3) all" << endl;
+    cout << "0) stop edit" << endl;
+    
+    string command;
+    enterData (command);
+    while (not(command =="1" or command=="2" or command=="3" or command =="0")){
+      cout << "Invalid command, please input it again: ";
+      cin >> command;
+    }
+  
+    if (command=="1"){
+      
+      //input range
+      double max, min;
+      int numberoutput=0;
+      while (numberoutput==0){
+        cout << "What is the range of the ammount?" << endl;
+        cout << "From: ";
+        cin >> min;
+        cout << "To: ";
+        cin >> max;
+
+        //check min and max
+        if(min > max){
+          double temp;
+          temp = max;
+          max = min;
+          min = temp; 
+        }
+
+        //output records
+        for (int i = 0; i < numberofdata; i++){
+          if(amount[i] >= min and amount[i] <= max){
+            cout << "Record number " << i+1 << ") " << amount[i] << ' ' << date[i] <<' '<< time[i] <<' '<< type[i] <<' '<< account[i] <<endl;
+            numberoutput++;
+          }
+        }
+      }
+      cout << "Which record you want to edit (please input the record number)? ";
+
+      cin >> recordtoedit;
+      recordtoedit--;
+      
+    }else if (command =="2"){
+      
+      //input target date
+      int numberoutput = 0;
+      while(numberoutput==0){
+        cout << "Please in put the date of the record you want to edit (8 digits): " ;
+        string targetdate;
+        cin >> targetdate;
+
+        //date must be 8 digits, cannot start with zero, can add some more condition later such as month<=12 and date<=31...
+        while (targetdate.length() != 8){
+          cout << endl;
+          cout << "Date must be 8 digits and cannot start with zero, please enter it again: ";
+          cin >> targetdate;
+        }
+
+        //output records
+      
+        for (int i = 0; i < numberofdata; i++){
+          if(date[i] == targetdate){
+            cout << "Record number " << i+1 << ") " << amount[i] << ' ' << date[i] <<' '<< time[i] <<' '<< type[i] <<' '<< account[i] <<endl;
+            numberoutput++;
+          }
+        }
+      }
+      cout << "Which record you want to edit (please input the record number)? ";
+      
+      cin >> recordtoedit;
+      recordtoedit--;
+      
+    }else if(command == "0"){
+      break;
+      
+    }else if (command == "3"){
+      for (int i = 0; i < numberofdata; i++){
+        cout << "Record number " << i+1 << ") " << amount[i] << ' ' << date[i] <<' '<< time[i] <<' '<< type[i] <<' '<< account[i] <<endl;
+     
+      }
+      cout << "Which record you want to edit (please input the record number)? ";
+      
+      cin >> recordtoedit;
+      recordtoedit--;
+    }
+    
+    //select edit column
+    cout << "amount: " << amount[ recordtoedit] << endl;
+    cout << "date: " << date[ recordtoedit] << endl;
+    cout << "time: " << time[ recordtoedit] << endl;
+    cout << "type: " << type[ recordtoedit] << endl;
+    cout << "account: " << account[ recordtoedit] <<endl;
+    cout << "Which column you want to edit from this record?" << endl;
+    cout << "1) amount" << endl;
+    cout << "2) date" << endl;
+    cout << "3) time" << endl;
+    cout << "4) type" << endl;
+    cout << "5) account" << endl;
+    string column;
+    cin >> column;
+    while (not(column =="1" or column=="2" or column=="3" or column=="4" or column=="5")){
+      cout << "Invalid column seleted, please input it again: ";
+      cin >> column;
+    }
+  
+    //edit the column seleted
+    if (column == "1"){
+      double newamount;
+      cout << "Please enter the new amount (possitive if income, negative if expense): ";
+      cin >> newamount;
+      while (newamount == 0) {
+        cout << endl;
+        cout << "Amount cannot be zero, please enter it again: ";
+        cin >> newamount;
+      }
+      amount[ recordtoedit] = newamount;
+      
+    }else if (column  =="2"){
+      string newdate;
+      cout << "Please enter the new date (eg. please enter 20190423 if the date is 2019/4/23): ";
+      cin >> newdate;
+      //date must be 8 digits, cannot start with zero, can add some more condition later such as month<=12 and date<=31...
+      while (newdate.length() != 8){
+        cout << endl;
+        cout << "Date must be 8 digits and cannot start with zero, please enter it again: ";
+        cin >> newdate;
+      }
+      date[recordtoedit]= newdate;
+      
+    }else if (column =="3"){
+      string newtime;
+      cout << "Please enter the new time (eg. please enter 1612 if the time is 4:12pm and 0230 if the time is 2:30am): ";
+      cin >> newtime;
+      //time must be 4 digits, can add some more condition later such as hr<24 and mins<60...
+      while (newtime.length() != 4) { //or stoi(time.substr(0,2)) >= 24 or stoi(time.substr(2,2)) >= 60){
+        cout << endl;
+
+        // test
+        cout << newtime << endl;
+        cout << "Length: " << newtime.length() << endl;
+
+        cout << "Time must be in 4 digit and , please enter it again: ";
+        cin >> newtime;
+      }
+      time[recordtoedit] = newtime;
+      
+    }else if (column =="4"){
+      string newtype;
+      cout << "Please enter the new type in one word (eg. food, wage...): ";
+      enterData(newtype);
+      while (containsString(newtype, " ")) {
+          cout << "Type cannot contain space, please enter it again." << endl;
+          cout << "Please enter type in one word (eg. food, wage...): ";
+          enterData(newtype);
+      }
+      type[recordtoedit] = newtype;
+      
+    }else if (column =="5"){
+      string newaccount;
+      cout << "Please enter the new account in one word (eg. cash, octopus, credit_card...): ";
+      enterData(newaccount);
+      while (containsString(newaccount, " ")) {
+          cout << "Account cannot contain space, please enter it again." << endl;
+          cout << "Please enter account in one word (eg. cash, octopus, credit_card...): ";
+          enterData(newaccount);
+      }
+      account[recordtoedit] = newaccount;
+    }
+    
+    
+  
+    cout << "Record edited successfully" << endl;
+  }
+  
+  //rewrite username.txt
+  ofstream fout(username + ".txt");
+  fout << numberofdata <<endl;
+  for (int i=0; i<numberofdata; i++){
+    fout << amount[i] << ' ' << date[i] <<' '<< time[i] <<' '<< type[i] <<' '<< account[i] <<endl;
+  }
+  fout.close();
+ 
+  //delete dynamic array
+  delete [] amount;
+  delete [] date;
+  delete [] time;
+  delete [] type;
+  delete [] account;
 }
 
 // ================================
@@ -363,6 +591,105 @@ void mode_edit(string username) {
 // Input: string username: user's username
 void mode_view(string username) {
   cout << "VIEW mode selected" << endl;
+  
+  ifstream fin(username + ".txt");
+  int numberofdata;
+  fin >> numberofdata;
+  
+  //create 4 dynamic arrays
+  double * amount = new double [numberofdata];
+  string * date = new string [numberofdata];
+  string * time = new string [numberofdata];
+  string * type = new string [numberofdata];
+  string * account = new string [numberofdata];
+  
+  //input all data into array
+  for (int i=0; i<numberofdata; i++){
+    fin >> amount[i];
+    fin >> date[i];
+    fin >> time[i];
+    fin >> type[i];
+    fin >> account[i];  
+  }
+  fin.close();
+  
+  //select and test the command
+  cout << "View the record by" << endl;
+  cout << "1) amount" << endl;
+  cout << "2) date" << endl;
+  cout << "3) all" << endl;
+  string command;
+  cin >> command;
+  
+  while (not(command =="1" or command=="2" or command=="3")){
+    cout << "Invalid command, please input it again: ";
+    cin >> command;
+  }
+  
+  //print output
+  if (command == "1"){
+    double max, min;
+    int numberoutput=0;
+    while (numberoutput==0){
+      cout << "What is the range of the ammount?" << endl;
+      cout << "From: ";
+      cin >> min;
+      cout << "To: ";
+      cin >> max;
+
+      //check min and max
+      if(min > max){
+        double temp;
+        temp = max;
+        max = min;
+        min = temp; 
+      }
+
+      //output records
+      for (int i = 0; i < numberofdata; i++){
+        if(amount[i] >= min and amount[i] <= max){
+          cout << "Record number " << i+1 << ") " << amount[i] << ' ' << date[i] <<' '<< time[i] <<' '<< type[i] <<' '<< account[i] <<endl;
+          numberoutput++;
+        }
+      }
+    }
+  }else if(command == "2"){
+    
+      int numberoutput = 0;
+    while(numberoutput==0){
+      cout << "Please in put the date of the record you want to view (8 digits): " ;
+      string targetdate;
+      cin >> targetdate;
+      
+      //date must be 8 digits, cannot start with zero, can add some more condition later such as month<=12 and date<=31...
+      while (targetdate.length() != 8){
+        cout << endl;
+        cout << "Date must be 8 digits and cannot start with zero, please enter it again: ";
+        cin >> targetdate;
+      }
+
+     //output records
+     
+      for (int i = 0; i < numberofdata; i++){
+        if(date[i] == targetdate){
+          cout << "Record number " << i+1 << ") " << amount[i] << ' ' << date[i] <<' '<< time[i] <<' '<< type[i] <<' '<< account[i] <<endl;
+          numberoutput++;
+        }
+      }
+    }
+  }else if(command == "3"){
+    for (int i = 0; i < numberofdata; i++){   
+      cout << "Record number " << i+1 << ") " << amount[i] << ' ' << date[i] <<' '<< time[i] <<' '<< type[i] <<' '<< account[i] <<endl;
+    }  
+  } 
+  
+  
+  //delete dynamic array
+  delete [] amount;
+  delete [] date;
+  delete [] time;
+  delete [] type;
+  delete [] account;
 }
 
 // ================================
@@ -596,7 +923,7 @@ int main() {
     enterMode(mode, valid_modes, mode_size);
   }
 
-  cout << "Exiting..." << endl;
+  cout << "Exiting... Thank you" << endl;
       
   // ^---------------------^
 
